3509e11f2d8bd2d0d3b0b67c19cd3d4eff9db0dd
Senorine <fabiansenorine@hotmail.com>
2021-01-06 20:20:43 -0500

    first commit

---
+++ src/main/scala/example/ClassDemo.scala
@@ -0,0 +1,17 @@
+package example
+
+object ClassDemo extends App{
+
+    
+        def myMethod(): Unit =
+        {
+            println("Helloo");2
+            
+
+        }
+
+    
+
+    
+}
+
---
+++ src/main/scala/example/Hello.scala
@@ -0,0 +1,13 @@
+package example
+import scala.io.StdIn.readLine
+import scala.io.StdIn.readInt
+
+import example.ClassDemo;
+
+object Hello extends  App {
+
+    for(p <- 1 to 5) println(p)
+
+
+
+}
